4d38be3dff3544c6fd51a2a8fc4685b053e4ad4e
Hazem <hazem.moh98@gmail.com>
2021-06-14 20:40:40 +0200

    view and change unknown faces in mobile app

--- Software/Face Detecton/FaceUnlock.py
+++ Software/Face Detecton/FaceUnlock.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import face_recognition
 import pickle
@@ -6,7 +7,7 @@ import paho.mqtt.client as mqtt
 from datetime import datetime, timedelta
 import glob
 
-# TODO: Fix unknown faces
+import base64
 
 
 def findFace():
@@ -26,14 +27,17 @@ def findFace():
         known_face_encodings.append(img_encoding)
         known_face_names.append(file)
 
+    files = []
+    for file in glob.glob("unknown/*.jpg"):
+        files.append(file)
+
     unknown_face_encodings = []
-    with open('unknown/unknown_face_encodings.dat', 'rb') as f:
-        try:  # Check if there are previous saved encodings
-            unknown_face_encodings = pickle.load(f)
-            count_number = len(unknown_face_encodings)
-        except:
-            count_number = 0
-            unknown_face_encodings = []
+    for file in files:
+        img = face_recognition.load_image_file(file)
+        img_encoding = face_recognition.face_encodings(img)[0]
+        unknown_face_encodings.append(img_encoding)
+
+    count_number = len(unknown_face_encodings)
 
     print('We have : ', count_number, 'Unknown Images')
 
@@ -63,19 +67,24 @@ def findFace():
             name = "Unknown"
 
             # If a match was found in known_face_encodings, just use the first one.
-            if True in matches:
-                #     first_match_index = matches.index(True)
-                #     name = known_face_names[first_match_index]
-                # Publish to MQTT server to open Door
+            # if True in matches:
+            #    #     first_match_index = matches.index(True)
+            #    #     name = known_face_names[first_match_index]
+            #    # Publish to MQTT server to open Door
+            #    client.publish("Door/State", "1")
+            #    cv2.destroyAllWindows()
+            #    return
+
+            # Or instead, use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(
+                known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+                print("UNLOCKED DOOR TO ", name)
                 client.publish("Door/State", "1")
                 cv2.destroyAllWindows()
                 return
-
-            # Or instead, use the known face with the smallest distance to the new face
-            #face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            #best_match_index = np.argmin(face_distances)
-            # if matches[best_match_index]:
-                #name = known_face_names[best_match_index]
 
             else:
                 unmatches = face_recognition.compare_faces(
@@ -88,22 +97,15 @@ def findFace():
                         unknown_face_encodings.append(face_encoding)
                         fileName = 'unknown/' + str(
                             count_number) + '.jpg'
-                        roiGray = rgb_frame[top:bottom, left:right]
-                        cv2.imwrite(fileName, roiGray)
+                        cv2.imwrite(fileName, frame)
                         count_number += 1
-                        with open('unknown/unknown_face_encodings.dat',
-                                  'wb') as f:
-                            pickle.dump(unknown_face_encodings, f)
+
                 else:
                     unknown_face_encodings.append(face_encoding)
                     fileName = 'unknown/' + \
                         str(count_number) + '.jpg'
-                    roiGray = rgb_frame[top:bottom, left:right]
-                    cv2.imwrite(fileName, roiGray)
+                    cv2.imwrite(fileName, frame)
                     count_number += 1
-                    with open('unknown/unknown_face_encodings.dat',
-                              'wb') as f:
-                        pickle.dump(unknown_face_encodings, f)
 
             # Draw a box around the face
             cv2.rectangle(frame, (left - 20, top - 20),
@@ -121,15 +123,72 @@ def findFace():
             cv2.destroyAllWindows()
 
 
+def sendPhotos():
+    for file in glob.glob("unknown/*.jpg"):
+        f = open(file, "rb")
+        fileContent = f.read()
+        byteArr = base64.encodebytes(fileContent)
+        byteArr = byteArr.strip()
+        byteArr = byteArr.decode()
+        byteArr.replace('\n', '')
+
+        client.publish("Door/Photos", byteArr)
+
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code ", str(rc))
     client.subscribe('Door/PIR')
+    client.subscribe('Door/requestPhotos')
+    client.subscribe('Door/Approve')
+    client.subscribe('Door/Delete')
+
+
+def Approve(index):
+    print("Approving user ", index)
+
+    files = []
+    for file in glob.glob("unknown/*.jpg"):
+        files.append(file)
+
+    index = int(index)
+    mahindex = 0
+    
+    for file in files:
+        if(mahindex == index):
+            file = str(file)
+            newfile = file.replace("unknown\\", "")
+            print(newfile)
+            os.rename(file, "known\\" + newfile)  # DELETE IMAGE
+
+        mahindex += 1
+
+
+def Delete(index):
+    print("Deleting user ", index)
+
+    files = []
+    for file in glob.glob("unknown/*.jpg"):
+        files.append(file)
+
+    mahindex = 0
+    index = int(index)
+    for file in files:
+        if(mahindex == index):
+            os.remove(file)  # DELETE IMAGE
+        mahindex += 1
 
 
 def on_message(client, userdata, msg):
     if(msg.topic == 'Door/PIR'):
         if(msg.payload.decode("utf-8") == '1'):
             findFace()
+    if(msg.topic == 'Door/requestPhotos'):
+        if(msg.payload.decode("utf-8") == '1'):
+            sendPhotos()
+    if(msg.topic == 'Door/Approve'):
+        Approve(msg.payload.decode("utf-8"))
+    if(msg.topic == 'Door/Delete'):
+        Delete(msg.payload.decode("utf-8"))
 
 
 broker_address = "192.168.1.19"
